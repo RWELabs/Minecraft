772aeb594f05f857b9bc8797e74c800d2e816eaa
RWELabs <ryan.walpole@outlook.com>
2022-11-14 01:53:14 +1100

    Update Version Number
    Update version number within the internal app settings.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

--- Software/Source Code/World-Saver/TBP World Saver/Properties/Settings.Designer.cs
+++ Software/Source Code/World-Saver/TBP World Saver/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace TBP_World_Saver.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace TBP_World_Saver.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.1")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.2")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
