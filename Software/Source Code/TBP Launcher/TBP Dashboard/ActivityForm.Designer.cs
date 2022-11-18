a03d160d6f250e709393f17936f5df177992bce6
Ryan Walpole <ryan.walpole@outlook.com>
2022-11-18 15:01:10 +1100

    Create Window (ActivityForm)
    Create form that opens to indicate launch activity for the Minecraft Launcher.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ Software/Source Code/TBP Launcher/TBP Dashboard/ActivityForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace TBP_Dashboard
+{
+    partial class ActivityForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ActivityForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CloseWindow = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.GameNotInstalled = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TBP_Dashboard.Properties.Resources._285;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 159);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(415, 64);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CloseWindow
+            // 
+            this.CloseWindow.Interval = 5000;
+            this.CloseWindow.Tick += new System.EventHandler(this.CloseWindow_Tick);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.label1.Location = new System.Drawing.Point(0, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Attempting to Launch Minecraft...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // GameNotInstalled
+            // 
+            this.GameNotInstalled.Interval = 20000;
+            this.GameNotInstalled.Tick += new System.EventHandler(this.GameNotInstalled_Tick);
+            // 
+            // ActivityForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(414, 391);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ActivityForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Opening Minecraft...";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ActivityForm_FormClosed);
+            this.Load += new System.EventHandler(this.ActivityForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer CloseWindow;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer GameNotInstalled;
+    }
+}
---
+++ Software/Source Code/TBP Launcher/TBP Dashboard/ActivityForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TBP_Dashboard
+{
+    public partial class ActivityForm : Form
+    {
+        public ActivityForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseWindow_Tick(object sender, EventArgs e)
+        {
+            int counter = 0;
+            foreach (Process process in Process.GetProcessesByName("Minecraft Launcher (32 bit)"))
+            {
+                counter++;
+                //MessageBox.Show("MC32");
+            }
+            foreach (Process process in Process.GetProcessesByName("Minecraft"))
+            {
+                counter++;
+                //MessageBox.Show("MC");
+            }
+            foreach (Process process in Process.GetProcessesByName("MinecraftLauncher"))
+            {
+                counter++;
+                //MessageBox.Show("MCL");
+            }
+
+            if (counter > 0)
+            {
+                CloseWindow.Stop();
+                this.Close();
+            }
+            else
+            {
+                //game not running - assume not installed
+                GameNotInstalled.Start();
+            }
+
+        }
+
+        private void ActivityForm_Load(object sender, EventArgs e)
+        {
+            CloseWindow.Start();
+        }
+
+        private void GameNotInstalled_Tick(object sender, EventArgs e)
+        {
+            GameNotInstalled.Stop();
+            this.Close();
+        }
+
+        private void ActivityForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CloseWindow.Stop();
+            GameNotInstalled.Stop();
+        }
+    }
+}
