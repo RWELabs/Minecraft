bfdd939b22aabeb4e79c6945001551eafd6a792c
Ryan Walpole <ryan.walpole@outlook.com>
2022-12-05 10:49:48 +1100

    Create AssemblyInfo
    Create file with Assembly Information for main executable.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ Software/Source Code/TBP Launcher/TBP Dashboard/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TBP Launcher")]
+[assembly: AssemblyDescription("Access news, events, downloads, world saves and more with the TBP Launcher.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RWE Labs, under exclusive license from Crutionix Media Group, Ryan Walpole Enterprises")]
+[assembly: AssemblyProduct("TBP Launcher")]
+[assembly: AssemblyCopyright("Copyright © Crutionix Media Group, Ryan Walpole Enterprises - 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("07f73b84-2376-42fa-ba70-0f0632253405")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("22.11.0.1")]
+[assembly: AssemblyFileVersion("22.11.0.1")]
+[assembly: NeutralResourcesLanguage("en-AU")]
