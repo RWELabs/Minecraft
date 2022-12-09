8ef42f34b767a5cd98091f26bb6ddc7d32432a74
Ryan Walpole <ryan.walpole@outlook.com>
2022-12-09 11:11:20 +1100

    Create Launch Code
    Create launch code for application.
    
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>

---
+++ Software/Source Code/TBP Launcher/TBP Dashboard/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TBP_Dashboard
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            try
+            {
+                string WV = CoreWebView2Environment.GetAvailableBrowserVersionString();
+                
+                if(WV != null)
+                {
+                    Application.Run(new SignIn());
+                }
+                else
+                {
+                    Application.Run(new FirstRunSetup());
+                }
+            }
+            catch
+            {
+                Application.Run(new FirstRunSetup());
+            }
+        }
+    }
+}
