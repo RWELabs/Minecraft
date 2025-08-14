abe2f6cc5643fa545a401739e30b6b21c22e5a5b
Ryan Walpole <ryan.walpole@outlook.com>
2025-08-14 14:03:47 +1000

    Update UI (Activity Form)
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>
    Co-Authored-By: Crutionix <54631790+Crutionix@users.noreply.github.com>

--- Software/Source Code/TBP Launcher/TBP Dashboard/ActivityForm.Designer.cs
+++ Software/Source Code/TBP Launcher/TBP Dashboard/ActivityForm.Designer.cs
@@ -30,56 +30,64 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ActivityForm));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.CloseWindow = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
             this.GameNotInstalled = new System.Windows.Forms.Timer(this.components);
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Image = global::TBP_Dashboard.Properties.Resources._285;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 159);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(415, 64);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
             // 
             // CloseWindow
             // 
             this.CloseWindow.Interval = 5000;
             this.CloseWindow.Tick += new System.EventHandler(this.CloseWindow_Tick);
             // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F);
-            this.label1.Location = new System.Drawing.Point(0, 234);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(415, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Attempting to Launch Minecraft...";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // GameNotInstalled
             // 
             this.GameNotInstalled.Interval = 20000;
             this.GameNotInstalled.Tick += new System.EventHandler(this.GameNotInstalled_Tick);
             // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Font = new System.Drawing.Font("Segoe UI Semilight", 11.25F);
+            this.StatusLabel.ForeColor = System.Drawing.Color.White;
+            this.StatusLabel.Location = new System.Drawing.Point(40, 158);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(335, 23);
+            this.StatusLabel.TabIndex = 6;
+            this.StatusLabel.Text = "Attempting to launch Minecraft";
+            this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(40, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 27);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Working on it...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TBP_Dashboard.Properties.Resources._2851;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 209);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(362, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // ActivityForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.White;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
             this.ClientSize = new System.Drawing.Size(414, 391);
             this.ControlBox = false;
+            this.Controls.Add(this.StatusLabel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -97,10 +105,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer CloseWindow;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer GameNotInstalled;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
