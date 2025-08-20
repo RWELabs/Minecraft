af8117bc6cf889d538ed8054c72a00493fe3412f
Ryan Walpole <ryan.walpole@outlook.com>
2025-08-20 16:22:56 +1000

    Update to 250802 in Settings
    Co-Authored-By: Ryan Walpole <69621127+RyanWalpole@users.noreply.github.com>
    Co-Authored-By: Crutionix <54631790+Crutionix@users.noreply.github.com>

--- Software/Source Code/TBP Launcher/TBP Dashboard/Properties/Settings.Designer.cs
+++ Software/Source Code/TBP Launcher/TBP Dashboard/Properties/Settings.Designer.cs
@@ -74,7 +74,7 @@ namespace TBP_Dashboard.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("250801")]
+        [global::System.Configuration.DefaultSettingValueAttribute("250802")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
